3e883c870bbc7f775a1abbefffffbca2303b47a3
thomas.firmin19@gmail.com <thomas.firmin19@gmail.com>
2024-03-04 18:47:52 +0100

    Added back in time

--- zellij/strategies/tools/turbo_state.py
+++ zellij/strategies/tools/turbo_state.py
@@ -53,42 +53,6 @@ class TurboState:
         )
 
 
-@dataclass
-class CTurboState:
-    dim: int
-    batch_size: int
-    best_constraint_values: Tensor
-    length: float = 1.0
-    length_min: float = 0.5**7
-    length_max: float = 1.6
-    failure_counter: int = 0
-    failure_tolerance: int = float("nan")  # type: ignore
-    success_counter: int = 0
-    success_tolerance: int = 3  # Note: The original paper uses 3
-    best_value: float = -float("inf")
-    restart_triggered: bool = False
-
-    def __post_init__(self):
-        self.failure_tolerance = math.ceil(
-            max([4.0 / self.batch_size, float(self.dim) / self.batch_size])
-        )
-
-    def reset(self):
-        self.best_constraint_values *= torch.inf
-        self.length = 0.8
-        self.length_min = 0.5**7
-        self.length_max = 1.6
-        self.failure_counter = 0
-        self.success_counter = 0
-        self.success_tolerance = 3  # Note: The original paper uses 3
-        self.best_value = -float("inf")
-        self.restart_triggered = False
-
-        self.failure_tolerance = math.ceil(
-            max([4.0 / self.batch_size, float(self.dim) / self.batch_size])
-        )
-
-
 def update_state(state, Y_next):
     if max(Y_next) > state.best_value + 1e-3 * math.fabs(state.best_value):
         state.success_counter += 1
@@ -126,6 +90,42 @@ def update_tr_length(state):
         state.restart_triggered = True
 
     return state
+
+
+@dataclass
+class CTurboState:
+    dim: int
+    batch_size: int
+    best_constraint_values: Tensor
+    length: float = 0.8
+    length_min: float = 0.5**7
+    length_max: float = 1.6
+    failure_counter: int = 0
+    failure_tolerance: int = float("nan")  # type: ignore
+    success_counter: int = 0
+    success_tolerance: int = 3  # Note: The original paper uses 3
+    best_value: float = -float("inf")
+    restart_triggered: bool = False
+
+    def __post_init__(self):
+        self.failure_tolerance = math.ceil(
+            max([4.0 / self.batch_size, float(self.dim) / self.batch_size])
+        )
+
+    def reset(self):
+        self.best_constraint_values *= torch.inf
+        self.length = 0.8
+        self.length_min = 0.5**7
+        self.length_max = 1.6
+        self.failure_counter = 0
+        self.success_counter = 0
+        self.success_tolerance = 3  # Note: The original paper uses 3
+        self.best_value = -float("inf")
+        self.restart_triggered = False
+
+        self.failure_tolerance = math.ceil(
+            max([4.0 / self.batch_size, float(self.dim) / self.batch_size])
+        )
 
 
 def update_c_state(state, Y_next, C_next):
@@ -176,6 +176,130 @@ def update_c_state(state, Y_next, C_next):
     # Finally, update the length of the trust region according to the
     # updated success and failure counters
     state = update_tr_length(state)
+    return state
+
+
+@dataclass
+class ICTurboState:
+    dim: int
+    batch_size: int
+    best_constraint_values: Tensor
+    length: float = 0.8
+    length_min: float = 0.5**7
+    length_max: float = 1.6
+    failure_counter: int = 0
+    failure_tolerance: int = 2
+    success_counter: int = 0
+    success_tolerance: int = 1
+    best_value: float = -float("inf")
+    restart_triggered: bool = False
+    current_lbounds: Optional[Tensor] = None
+    current_ubounds: Optional[Tensor] = None
+
+    def __post_init__(self):
+        self.current_lbounds = torch.zeros(self.dim)
+        self.current_ubounds = torch.ones(self.dim)
+
+    def reset(self):
+        self.best_constraint_values *= torch.inf
+        self.length = 0.8
+        self.length_min = 0.5**7
+        self.length_max = 1.6
+        self.failure_counter = 0
+        self.failure_tolerance = 2
+        self.success_counter = 0
+        self.success_tolerance = 1
+        self.best_value = -float("inf")
+        self.restart_triggered = False
+        self.current_lbounds = torch.zeros(self.dim)
+        self.current_ubounds = torch.ones(self.dim)
+
+
+def iupdate_c_state(state, X_next, Y_next, C_next, lengths, successes, failures):
+    # Determine which candidates meet the constraints (are valid)
+    bool_tensor = C_next <= 0
+    bool_tensor = torch.all(bool_tensor, dim=-1)
+    Valid_Y_next = Y_next[bool_tensor]
+    Valid_C_next = C_next[bool_tensor]
+    Valid_X_next = X_next[bool_tensor]
+
+    if Valid_Y_next.numel() == 0:  # if none of the candidates are valid
+        # pick the point with minimum violation
+        sum_violation = C_next.sum(dim=-1)
+        min_violation = sum_violation.min()
+        # if the minimum voilation candidate is smaller than the violation of the incumbent
+        if min_violation < state.best_constraint_values.sum():
+            # count a success and update the current best point and constraint values
+            state.success_counter += 1
+            state.failure_counter = 0
+            # new best is min violator
+            state.best_value = Y_next[sum_violation.argmin()].item()
+            state.best_constraint_values = C_next[sum_violation.argmin()]
+        else:
+            # otherwise, count a failure
+            state.success_counter = 0
+            state.failure_counter += 1
+    else:  # if at least one valid candidate was suggested,
+
+        in_tr_X = torch.all(
+            (Valid_X_next >= state.current_lbounds)
+            & (Valid_X_next <= state.current_ubounds),
+            dim=-1,
+        )
+
+        argmax = torch.argmax(Valid_Y_next)
+
+        # throw out all invalid candidates
+        # (a valid candidate is always better than an invalid one)
+
+        # Case 1: if the best valid candidate found has a higher objective value that
+        # incumbent best count a success, the obj values has been improved
+        improved_obj = Valid_Y_next[argmax] > state.best_value + 1e-3 * math.fabs(
+            state.best_value
+        )
+
+        greedy_move = False
+        # Case 2: if incumbent best violates constraints
+        # count a success, we now have suggested a point which is valid and thus better
+        obtained_validity = torch.all(state.best_constraint_values > 0)
+        if improved_obj or obtained_validity:  # If Case 1 or Case 2
+            # count a success and update the best value and constraint values
+
+            if in_tr_X[argmax]:
+                state.success_counter += 1
+                state.failure_counter = 0
+                state.best_value = Valid_Y_next[argmax].item()
+                state.best_constraint_values = Valid_C_next[Valid_Y_next[argmax]]
+            else:
+                state.success_counter = 0
+                state.failure_counter = 0
+                state.best_value = Valid_Y_next[argmax].item()
+                state.best_constraint_values = Valid_C_next[argmax]
+                greedy_move = True
+        else:
+            # otherwise, count a failure
+            state.success_counter = 0
+            state.failure_counter += 1
+
+    # Finally, update the length of the trust region according to the
+    # updated success and failure counters
+    # Update the length of the trust region according to
+    # success and failure counters
+    # (Just as in original TuRBO paper)
+    if greedy_move:
+        state.length = lengths[argmax]
+        state.success_counter = successes[argmax]
+        state.failure_counter = failures[argmax]
+    elif state.success_counter == state.success_tolerance:  # Expand trust region
+        state.length = min(2.0 * state.length, state.length_max)
+        state.success_counter = 0
+    elif state.failure_counter == state.failure_tolerance:  # Shrink trust region
+        state.length /= 2.0
+        state.failure_counter = 0
+
+    if state.length < state.length_min:  # Restart when trust region becomes too small
+        state.restart_triggered = True
+
     return state
 
 
