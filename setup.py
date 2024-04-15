59bd40d8fe837e964059af16857dae3789c0647c
thomas.firmin19@gmail.com <thomas.firmin19@gmail.com>
2024-04-15 10:10:07 +0200

    Update python version to 3.10

--- setup.py
+++ setup.py
@@ -3,11 +3,7 @@
 # @Email:  thomas.firmin@univ-lille.fr
 # @Project: Zellij
 # @Last modified by:   tfirmin
-<<<<<<< Updated upstream
 # @Last modified time: 2022-12-23T14:49:36+01:00
-=======
-# @Last modified time: 2022-10-03T22:39:31+02:00
->>>>>>> Stashed changes
 # @License: CeCILL-C (http://www.cecill.info/index.fr.html)
 
 import setuptools
@@ -70,5 +66,5 @@ setuptools.setup(
         "mpi": ["mpi4py>=3.1.2"],
         "docs": docs_extras,
     },
-    python_requires=">=3.6",
+    python_requires=">=3.10",
 )
